8a723b9ddbb76ff24d59b9d696970fb40051ccbc
sanjaii1 <bsanjay235@gmail.com>
2024-10-14 00:54:07 +0530

    connect API

--- backend/model/product.model.js
+++ backend/model/product.model.js
@@ -20,6 +20,4 @@ const productSchema = new mongoose.Schema(
   }
 );
 
-const Product = mongoose.model("Product", productSchema);
-
-export default Product;
+export const Product = mongoose.model('Product', productSchema);
--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,40 @@
 import express from "express";
 import { connectDB } from "./config/db.js";
 import dotenv from "dotenv";
+import { Product } from "./model/product.model.js";
 
 dotenv.config();
 
 const app = express();
 
-app.get("/products", (req, res) => {});
+app.use(express.json());
+
+app.post("/api/products", async (req, res) => {
+  const product = req.body;
+
+  if (!product.name || !product.price || !product.image) {
+    return res.status(400).json({
+      success: false,
+      message: "Please provide all fields",
+    });
+  }
+
+  const newProduct = new Product(product);
+
+  try {
+    await newProduct.save();
+    res.status(201).json({
+      success: true,
+      data: newProduct,
+    });
+  } catch (error) {
+    console.error("Error in creating product", error.message);
+    res.status(500).json({
+      success: false,
+      message: "Server Error",
+    });
+  }
+});
 
 app.listen(5000, () => {
   console.log("im server running http://localhost:5000/ here");
