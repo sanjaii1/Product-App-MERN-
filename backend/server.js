028300f52fe26c95c02e1b46afb97feba1f7df91
sanjaii1 <bsanjay235@gmail.com>
2024-10-15 22:59:30 +0530

    api GET POST

--- backend/server.js
+++ backend/server.js
@@ -2,12 +2,47 @@ import express from "express";
 import { connectDB } from "./config/db.js";
 import dotenv from "dotenv";
 import { Product } from "./model/product.model.js";
+import mongoose from "mongoose";
+
+
 
 dotenv.config();
 
 const app = express();
 
 app.use(express.json());
+
+app.get("/api/products", async (req, res) => {
+  try {
+    const products = await Product.find({});
+    res.status(200).json({ success: true, data: products });
+  } catch (error) {
+    console.log("error in fetching products:", error.message);
+    res.status(500).json({ success: false, message: "Server Error" });
+  }
+});
+
+app.put("/api/products/:id", async (req, res) => {
+  const { id } = req.params;
+
+  const product = req.body;
+
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    return res
+      .status(404)
+      .json({ success: false, message: "Invalid Product Id" });
+  }
+
+  try {
+    const updatedProduct = await Product.findByIdAndUpdate(id, product, {
+      new: true,
+    });
+
+    res.status(200).json({ success: true, data: updatedProduct });
+  } catch (error) {
+    res.status(500).json({ success: false, message: "Server Error" });
+  }
+});
 
 app.post("/api/products", async (req, res) => {
   const product = req.body;
@@ -36,20 +71,20 @@ app.post("/api/products", async (req, res) => {
   }
 });
 
-app.delete("/api/products/:id", async (req, res)=>{
-  const { id } = req.params
-  console.log("id:", id)
+app.delete("/api/products/:id", async (req, res) => {
+  const { id } = req.params;
+  console.log("id:", id);
 
-  try{
+  try {
     await Product.findByIdAndDelete(id);
-    res.status(200).json({success:true,message:"Product Delete"})
-  }catch(error){
+    res.status(200).json({ success: true, message: "Product Delete" });
+  } catch (error) {
     res.status(404).json({
       success: false,
       message: "Product Not found",
     });
   }
-})
+});
 
 app.listen(5000, () => {
   console.log("im server running http://localhost:5000/ here");
