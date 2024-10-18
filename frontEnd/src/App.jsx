1fbc1fd343e9b2d41991f86f118ba6a2f89d035a
sanjaii1 <bsanjay235@gmail.com>
2024-10-18 23:42:14 +0530

    base react page create

--- frontEnd/src/App.jsx
+++ frontEnd/src/App.jsx
@@ -1,11 +1,19 @@
-
+import { Box } from "@chakra-ui/react";
+import { Routes, Route } from "react-router-dom";
+import HomePage from "./pages/homePage";
+import CreatePage from "./pages/createPage";
+import Navbar from "./components/navbar";
 
 function App() {
-
   return (
-    <>
-    </>
-  )
+    <Box minH={"100vh"}>
+      <Navbar /> 
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/create" element={<CreatePage />} />
+      </Routes>
+    </Box>
+  );
 }
 
-export default App
+export default App;
---
+++ frontEnd/src/pages/createPage.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const CreatePage = () => {
+  return (
+    <div>createPage</div>
+  )
+}
+
+export default CreatePage
