845143e2dcc1e8f34bc86b300750b479e1b891cc
sanjaii1 <bsanjay235@gmail.com>
2024-10-23 23:24:58 +0530

    add page design

--- frontEnd/src/pages/createPage.jsx
+++ frontEnd/src/pages/createPage.jsx
@@ -1,9 +1,71 @@
-import React from 'react'
+import {
+  Box,
+  Button,
+  Container,
+  Heading,
+  Input,
+  useColorModeValue,
+  VStack,
+} from "@chakra-ui/react";
+import React, { useState } from "react";
 
 const CreatePage = () => {
-  return (
-    <div>createPage</div>
-  )
-}
+  const [newProduct, setNewProduct] = useState({
+    name: "",
+    price: "",
+    image: "",
+  });
 
-export default CreatePage
+  const handleAddProduct =() => {
+    console.log(newProduct)
+  }
+
+
+  return (
+    <Container maxW={"container.sm"}>
+      <VStack spacing={8}>
+        <Heading as={"h1"} size={"2xl"} textAlign={"center"}>
+          Create New Product
+        </Heading>
+        <Box
+          w={"full"}
+          bg={useColorModeValue("white", "gray.800")}
+          p={6}
+          shadow={"md"}
+        >
+          <VStack spacing={4}>
+            <Input
+              placeholder="Product Name"
+              name="name"
+              value={newProduct.name}
+              onChange={(e) =>
+                setNewProduct({ ...newProduct, name: e.target.value })
+              }
+            />
+            <Input
+              placeholder="Price"
+              name="price"
+              value={newProduct.price}
+              onChange={(e) =>
+                setNewProduct({ ...newProduct, price: e.target.value })
+              }
+            />{" "}
+            <Input
+              placeholder="Image URL"
+              name="image"
+              value={newProduct.image}
+              onChange={(e) =>
+                setNewProduct({ ...newProduct, image: e.target.value })
+              }
+            />
+            <Button colorScheme={"blue"} onClick={handleAddProduct} w="full">
+              Add Product
+            </Button>
+          </VStack>
+        </Box>
+      </VStack>
+    </Container>
+  );
+};
+
+export default CreatePage;
