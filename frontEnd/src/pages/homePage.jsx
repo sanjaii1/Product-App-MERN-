e01e00787a2141f611c2f5e8485eaac5a6222a3b
sanjaii1 <bsanjay235@gmail.com>
2024-10-25 00:11:50 +0530

    no data

--- frontEnd/src/pages/homePage.jsx
+++ frontEnd/src/pages/homePage.jsx
@@ -1,9 +1,51 @@
-import React from 'react'
+import React from "react";
+import {
+  Box,
+  Button,
+  Container,
+  Heading,
+  Input,
+ 
+  Text,
+  useColorModeValue,
+  useToast,
+  VStack,
+} from "@chakra-ui/react";
+import { Link } from "react-router-dom";
 
 const HomePage = () => {
   return (
-    <div>homePage</div>
-  )
-}
+    <Container maxW="container.xl" py={12}>
+      <VStack spacing={8}>
+        <Text
+          fontSize={"30"}
+          fontWeight={"bold"}
+          bgGradient={"linear(to-r,cyan.400, blue.500"}
+          textAlign={"center"}
+        >
+          Current Product
+        </Text>
 
-export default HomePage
+        <Text
+          fontSize="xl"
+          textAlign={"center"}
+          fontWeight={"bold"}
+          color="gray.500"
+        >
+          No product found
+          <Link to={"/create"}>
+          <Text as='span' color='blue.500' _hover={{textDecoration:"underline"}}>
+            Create Product
+          </Text>
+          
+          
+          
+          </Link>
+
+        </Text>
+      </VStack>
+    </Container>
+  );
+};
+
+export default HomePage;
