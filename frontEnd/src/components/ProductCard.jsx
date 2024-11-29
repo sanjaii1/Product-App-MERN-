4577e5812d75e528f440ab7e9e1c87272b2b9060
sanjaii1 <bsanjay235@gmail.com>
2024-11-29 23:21:49 +0530

    delete functionality implement

--- frontEnd/src/components/ProductCard.jsx
+++ frontEnd/src/components/ProductCard.jsx
@@ -6,12 +6,36 @@ import {
   IconButton,
   useColorModeValue,
   Image,
-  Text
+  Text,
+  useToast,
 } from "@chakra-ui/react";
 import React from "react";
+import { useProductStore } from "../store/product";
 const ProductCard = ({ product }) => {
   const textColor = useColorModeValue("gray.600", "gray.200");
   const bg = useColorModeValue("white", "gray.800");
+  const { deleteProduct } = useProductStore();
+  const toast = useToast;
+  const handleDeleteProduct = async (pid) => {
+    const { success, message } = await deleteProduct(pid);
+    if (!success) {
+      toast({
+        title: "Error",
+        description: message,
+        status: "error",
+        duration: 3000,
+        isClosable: true,
+      });
+    } else {
+      toast({
+        title: "Success",
+        description: message,
+        status: "success",
+        duration: 3000,
+        isClosable: true,
+      });
+    }
+  };
   return (
     <Box
       shadow="lg"
@@ -35,14 +59,20 @@ const ProductCard = ({ product }) => {
         <Heading as="h3" size="md" mb={2} color={textColor}>
           {product.name}
         </Heading>
-        <Text fontWeight='bold' fontSize='xl' color={textColor} mb={4}>${product.price}</Text>
+        <Text fontWeight="bold" fontSize="xl" color={textColor} mb={4}>
+          ${product.price}
+        </Text>
         <HStack spacing={2}>
           <IconButton icon={<EditIcon />} colorScheme="blue" />
-          <IconButton icon={<DeleteIcon />} colorScheme="red" />
+          <IconButton
+            icon={<DeleteIcon />}
+            onClick={() => handleDeleteProduct(product._id)}
+            colorScheme="red"
+          />
         </HStack>
       </Box>
     </Box>
   );
 };
 
-export default ProductCard; 
+export default ProductCard;
